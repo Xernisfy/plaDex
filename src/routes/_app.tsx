2c2bd27a60c0d62d55bb199dd4ff41fe3cab69b1
Xernisfy <xernisfy@gmail.com>
2025-04-10 10:15:35 +0200

    update dex, save and style

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -4,7 +4,7 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="height=device-height, width=device-width, initial-scale=0.7" />
         <title>Pokémon Legenden: Wolfgang</title>
         <link rel="icon" type="image/png" href="/assets/favicon.png" />
         <link rel="stylesheet" href="/scss/style.scss" />
