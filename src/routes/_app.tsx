1b25ee92d416a7512b328a6002dcdc6bf69078ad
Xernisfy <xernisfy@gmail.com>
2025-04-11 11:54:10 +0200

    fix mobile

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -4,6 +4,7 @@ export default function App({ Component }: PageProps) {
     <html>
       <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="initial-scale=0.8" />
         <title>Pokémon Legenden: Wolfgang</title>
         <link rel="icon" type="image/png" href="/assets/favicon.png" />
         <link rel="stylesheet" href="/scss/style.scss" />
